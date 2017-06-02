686984a3ffa78e45ee43f95b88744fcfc721aac5
Ram Manohar Bheemana <rbheemana@users.noreply.github.com>
2017-06-02 14:48:58 -0400

    Update TestCobolFieldFactory.java

--- src/com/savy3/hadoop/hive/serde3/cobol/TestCobolFieldFactory.java
+++ src/com/savy3/hadoop/hive/serde3/cobol/TestCobolFieldFactory.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import com.savy3.hadoop.hive.serde2.cobol.CobolSerdeException;
 public class TestCobolFieldFactory {
 	CobolFieldFactory cff = new CobolFieldFactory();
 
